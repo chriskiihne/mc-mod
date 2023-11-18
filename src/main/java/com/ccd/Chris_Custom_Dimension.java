17bd5b52d779398fef3ac8ceffed8ed16aafa6f9
Chris K. <kiihnechris@gmail.com>
2023-11-18 15:00:36 -0600

    init

--- src/main/java/com/ccd/Chris_Custom_Dimension.java
+++ src/main/java/com/ccd/Chris_Custom_Dimension.java
@@ -1,21 +1,15 @@
-package com.example;
+package com.ccd;
 
 import net.fabricmc.api.ModInitializer;
-import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.Items;
-import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class Chris_Custom_Dimension implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("chris_mod");
-	// public static final Block CUSTOM_BLOCK2 = new Block(FabricBlockSettings.create().strength(4f));
-	// public static final Block EXAMPLE_BLOCK2  = new Block(FabricBlockSettings.create().strength(4.0f));
+    public static final Logger LOGGER = LoggerFactory.getLogger("chris_custom_dimension");
 
 	@Override
 	public void onInitialize() {
@@ -24,6 +18,5 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
-
 	}
 }
---
+++ src/main/java/com/ccd/Chris_Custom_DimensionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ccd;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Chris_Custom_DimensionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
